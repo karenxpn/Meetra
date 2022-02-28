f7bb88224978b6855c1168f880dde3de2de01e7e
KALIMI <karenmirakyan@gmail.com>
2022-02-28 23:11:10 +0400

    Initial Commit

---
+++ Meetra/MeetraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetraApp.swift
+//  Meetra
+//
+//  Created by Karen Mirakyan on 28.02.22.
+//
+
+import SwiftUI
+
+@main
+struct MeetraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
