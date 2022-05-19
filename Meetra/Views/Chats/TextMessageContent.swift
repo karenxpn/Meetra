70e4eaa614d2e822a6eb650b8f7810fcbf916356
KALIMI <karenmirakyan@gmail.com>
2022-05-19 16:18:45 +0400

    link preview added

--- Meetra/Views/Chats/TextMessageContent.swift
+++ Meetra/Views/Chats/TextMessageContent.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import LinkPreview
 
 struct TextMessageContent: View {
     @AppStorage("userId") private var userID: Int = 0
@@ -17,6 +18,13 @@ struct TextMessageContent: View {
         if message.content.isSingleEmoji {
             Text(message.content)
                 .font(.system(size: 102))
+        } else if message.content.hasPrefix("https://") {
+            LinkPreview(url: URL(string: message.content))
+                .backgroundColor(message.sender.id == userID ? AppColors.accentColor : AppColors.addProfileImageBG)
+                .primaryFontColor(message.sender.id == userID ? .white : .black)
+                .secondaryFontColor(.white.opacity(0.6))
+                .titleLineLimit(3)
+                .frame(width: UIScreen.main.bounds.width * 0.5, alignment: message.sender.id == userID ? .trailing : .leading)
         } else {
             VStack( alignment: message.sender.id == userID && message.reptyedTo == nil ? .trailing : .leading) {
                 
@@ -36,7 +44,7 @@ struct TextMessageContent: View {
                     .foregroundColor(message.sender.id == userID ? .white : .black)
                     .font(.custom("Inter-Regular", size: 12))
                     .kerning(0.24)
-                    
+                
             }.padding(.vertical, 12)
                 .padding(.horizontal, 15)
                 .background(message.sender.id == userID ? AppColors.accentColor : AppColors.addProfileImageBG)
