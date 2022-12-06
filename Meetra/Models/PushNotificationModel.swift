f659c7461cc639cd0a6f43ca25f6820aaae0e96b
mbbrv <beburov1@gmail.com>
2022-12-06 12:51:13 +0300

    remove new properties push notification

--- Meetra/Models/PushNotificationModel.swift
+++ Meetra/Models/PushNotificationModel.swift
@@ -9,17 +9,4 @@ import Foundation
 struct PushNotificationModel: Codable {
     var title: String
     var content: String
-    var type: String
-    var chatId: Int
-    var user: PushNotificationUserPreview
-}
-
-struct PushNotificationUserPreview: Identifiable, Codable {
-    var id: Int
-    var name: String?
-    var online: Bool?
-    var lastVisit: String?
-    var image: String?
-    var blocked: Bool?
-    var blockedByMe: Bool?
 }
